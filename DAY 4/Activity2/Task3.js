00bbb18f79345348f0cd8171bdf4f7d554570051
Prakhar Singh <prakhar12330@gmail.com>
2024-07-16 13:35:10 +0530

    DAY-4 Task(s) Done

---
+++ DAY 4/Activity2/Task3.js
@@ -0,0 +1,7 @@
+let n = 1;
+let sum = 0;
+while (n <= 10) {
+    sum += n;
+    n++;
+}
+console.log(sum);
---
+++ DAY 4/Activity3/Task6.js
@@ -0,0 +1,8 @@
+let number = 11;
+let factorial=1;
+do {
+     factorial*=number;
+     number--;
+} while (number >1);
+
+console.log(`${factorial}`);
